c4783b0eb90ecb5b1e0ef424065e069ae91764aa
Léo Mendes <contato.leonardomendesgomes@gmail.com>
2024-10-04 12:15:05 -0300

    Add files via upload

---
+++ PRÁTICA 02/Exercicios Desvio Condicional e Operadores Relacionais e Lógicos/solucao10c.java
@@ -0,0 +1,13 @@
+ import javax.swing.JOptionPane;
+
+  public class solucao10c {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+
+        if (numero % 5 == 0) {
+            JOptionPane.showMessageDialog(null, numero + " é divisível por 5.");
+        } else {
+            JOptionPane.showMessageDialog(null, numero + " não é divisível por 5.");
+        }
+    }
+}
---
+++ PRÁTICA 02/Exercicios Desvio Condicional e Operadores Relacionais e Lógicos/solucao16c.java
@@ -0,0 +1,14 @@
+ import javax.swing.JOptionPane;
+
+  public class solucao16c {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
+        int b = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
+
+        if (a == b) {
+            JOptionPane.showMessageDialog(null, "Os números são iguais.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Os números são diferentes.");
+        }
+    }
+}
