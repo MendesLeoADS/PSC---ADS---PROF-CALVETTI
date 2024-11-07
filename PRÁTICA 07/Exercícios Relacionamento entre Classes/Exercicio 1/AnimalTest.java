83cad37a8437dedaf0f74ca28da942b449eb153a
Léo Mendes <contato.leonardomendesgomes@gmail.com>
2024-11-07 17:53:25 -0300

    Add files via upload

---
+++ PRÁTICA 07/Exercícios Relacionamento entre Classes/Exercicio 1/AnimalTest.java
@@ -0,0 +1,16 @@
+class AnimalTest {
+
+    void testMover() {
+
+        Animal animal = new Animal();
+
+        animal.mover();
+    }
+
+    void testLatir() {
+
+        Cachorro cachorro = new Cachorro();
+
+        cachorro.latir();
+    }
+}
